a453874a4f8c6032c58fc59114aa67f73338b621
Bonnie <Bonnie>
2025-06-05 14:53:33 +0300

    adds js code

---
+++ index.js
@@ -0,0 +1,12 @@
+//function expresion
+const divide = function () {
+  const x = 2000;
+  const y = 100;
+  return x / y;
+};
+
+//arrow function square
+const square = (x) => x ** 2;
+
+//arrow function add
+const add = (a, b) => a + b;
